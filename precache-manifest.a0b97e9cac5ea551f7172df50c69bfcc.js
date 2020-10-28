fbedc9f30aca6b48d8017038333d0815250ffc63
Yeff Espinoza <espinozayeff.git@gmail.com>
2020-10-27 23:44:38 -0500

    Updates

---
+++ precache-manifest.a0b97e9cac5ea551f7172df50c69bfcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073e329778576ebe5f18038820eab634",
+    "url": "/currency-chat/index.html"
+  },
+  {
+    "revision": "60f72131259a497e3fb2",
+    "url": "/currency-chat/static/css/main.71f66383.chunk.css"
+  },
+  {
+    "revision": "c41bb6c4c0df513aec38",
+    "url": "/currency-chat/static/js/2.5cfd42d6.chunk.js"
+  },
+  {
+    "revision": "8fbfd049ec4a4e26b3fa8cff9b5a58da",
+    "url": "/currency-chat/static/js/2.5cfd42d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60f72131259a497e3fb2",
+    "url": "/currency-chat/static/js/main.45d29985.chunk.js"
+  },
+  {
+    "revision": "0648e82a0c7d30c78220",
+    "url": "/currency-chat/static/js/runtime-main.d8b8e120.js"
+  }
+]);
